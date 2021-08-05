d7fb3548df361630b3d3546213902377142c2c1c
mshields <mshields@nrel.gov>
2021-08-05 16:22:37 -0600

    Added CSV output and ability to report percent change from initial Capex

--- FORCE/utility/plot_learning.py
+++ FORCE/utility/plot_learning.py
@@ -6,11 +6,12 @@ __status__ = "Development"
 
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 
 def plot_learning_forecast(
-    installed, capex, fit, forecast, bse=None, axes=None, **kwargs
+    installed, capex, fit, forecast, bse=None, axes=None, perc_change=False, **kwargs
 ):
     """
     Plots forecasted CAPEX/kW based on the installed capacity, current capex,
@@ -47,7 +48,18 @@ def plot_learning_forecast(
     x = np.linspace(installed, upcoming[-1])
     b0 = fit
     C0_0 = capex / (installed ** b0)
-    y0 = C0_0 * x ** b0
+    # # y0 = C0_0 * x ** b0
+    # y0 = 1 - C0_0 * x ** b0 / capex
+    # y_per_year = 1 - C0_0 * upcoming ** b0 / capex
+
+    if perc_change is False:
+        y0 = calc_curve(x, C0_0, b0)
+        y0_per_year = calc_curve(upcoming, C0_0, b0)
+        _out_col = 'Average global CapEx, $/KW'
+    else:
+        y0 = calc_curve(x, C0_0, b0, capex_0=capex)
+        y0_per_year = calc_curve(upcoming, C0_0, b0, capex_0=capex)
+        _out_col = 'Percent change from initial CapEx'
 
     ax1.plot(x, y0, "k-")
     ax1.set_xlabel("Cumulative Capacity")
@@ -60,9 +72,14 @@ def plot_learning_forecast(
         C0_1 = capex / (installed ** b1)
         C0_2 = capex / (installed ** b2)
 
-        y1 = C0_1 * x ** b1
-        y2 = C0_2 * x ** b2
-
+        # y1 = C0_1 * x ** b1
+        # y2 = C0_2 * x ** b2
+        if perc_change is False:
+            y1 = calc_curve(x, C0_1, b1)
+            y2 = calc_curve(x, C0_2, b2)
+        else:
+            y1 = calc_curve(x, C0_1, b1, capex_0=capex)
+            y2 = calc_curve(x, C0_2, b2, capex_0=capex)
         ax1.fill_between(x, y1, y2)
 
     ax2.set_xlim(ax1.get_xlim())
@@ -70,4 +87,20 @@ def plot_learning_forecast(
     ax2.set_xticklabels(forecast.keys(), rotation=45, fontsize=8)
     ax2.set_ylabel("Projected COD")
 
+    _out = pd.DataFrame({
+        'Year': forecast.keys(),
+        _out_col: y0_per_year
+    })
+
+    _out.to_csv('Projected_capex.csv')
     return ax1, ax2
+
+def calc_curve(x, C0, b, capex_0=None):
+    """Fit the learning curve to a prescribed range of years"""
+    if capex_0:
+        """Determine percent change from initial capex value"""
+        y = 1 - C0 * x ** b / capex_0
+    else:
+        y = C0 * x ** b
+
+    return y
